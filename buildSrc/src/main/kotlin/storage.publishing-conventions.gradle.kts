5209fc8ff052079e48b36c79b2e8980dcefe79de
Angel Miranda <pixeldev1729@gmail.com>
2023-02-18 19:39:51 -0600

    build: write project repositories on publication

--- buildSrc/src/main/kotlin/storage.publishing-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/storage.publishing-conventions.gradle.kts
@@ -6,6 +6,26 @@ plugins {
 publishing {
   publications {
     create<MavenPublication>("maven") {
+      pom.withXml {
+        val repositoriesNode = asNode().appendNode("repositories")
+        var index = 0
+
+        project.repositories.forEach {
+          val repositoryName = it.name
+
+          if (repositoryName == "MavenLocal" || repositoryName == "MavenRepo") {
+            return@forEach
+          }
+
+          repositoriesNode.appendNode("repository").apply {
+            val id = if (repositoryName == "maven") "repository-${++index}" else repositoryName
+
+            appendNode("id", id)
+            appendNode("url", (it as MavenArtifactRepository).url.toString())
+          }
+        }
+      }
+
       from(components["java"])
     }
   }
